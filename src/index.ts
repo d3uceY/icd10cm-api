4b6fa1d9d2f52f5306cbc8bdbc550ba43ab11f57
d3uceY <onyekwelujesse1234@gmail.com>
2026-03-28 16:05:39 +0100

    feat: load ICD codes during server startup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import app from './app';
 import config from './config/env';
+import { loadICD } from './services/icd.services';
 
 const server = app.listen(config.port, () => {
-  console.log(`[server] Running in ${config.nodeEnv} mode on port ${config.port}`);
+    loadICD();
+    console.log(`[server] Running in ${config.nodeEnv} mode on port ${config.port}`);
 });
 
 // Graceful shutdown
 process.on('SIGTERM', () => {
-  console.log('[server] SIGTERM received. Shutting down gracefully...');
-  server.close(() => {
-    console.log('[server] Process terminated.');
-    process.exit(0);
-  });
+    console.log('[server] SIGTERM received. Shutting down gracefully...');
+    server.close(() => {
+        console.log('[server] Process terminated.');
+        process.exit(0);
+    });
 });
 
 export default server;
